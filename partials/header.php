f8ed4189671b41c1a50b9b5e53ca88c190c92941
Mike Oshadami <oshadami.mj@gmail.com>
2018-03-06 15:33:28 +0100

    Added New Files for Layout

---
+++ partials/header.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>Startmin - Bootstrap Admin Theme</title>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="css/startmin.css" rel="stylesheet">
+
+<!-- Custom Fonts -->
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
---
+++ partials/side-nav.php
@@ -0,0 +1,26 @@
+<div class="navbar-default sidebar" role="navigation">
+    <div class="sidebar-nav navbar-collapse">
+        <ul class="nav" id="side-menu">
+            <li>
+                <a href="home.php" class="active"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Account<span class="fa arrow"></span></a>
+
+            </li>
+            <li>
+                <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Payment</a>
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-wrench fa-fw"></i> Transfer<span class="fa arrow"></span></a>
+
+                <!-- /.nav-second-level -->
+            </li>
+
+            <li>
+                <a href="index.php"><i class="fa fa-files-o fa-fw"></i> Logout<span class="fa arrow"></span></a>
+
+            </li>
+        </ul>
+    </div>
+</div>
